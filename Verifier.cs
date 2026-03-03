0f80e07959c36c625ec1e9eca440f035c9ec84f9
Thane <thabay0606@students.maquoketaschools.org>
2026-03-03 14:25:34 -0600

    Pretty sure I did it.

--- Verifier.cs
+++ Verifier.cs
@@ -2,4 +2,24 @@
 public class Verifier
 {
     // Add your code here
+    public string Taxable(string state, bool taxExempt){
+        if (state == "IA" && taxExempt == false){
+            return "is taxable";
+        }
+        else if (state == "IA" && taxExempt == true){
+            return "";
+        }
+        else (state == "FL" && taxExempt == false){
+            return "";
+        }
+    }
+    
+    public string IsVehicle(bool hasWheels, bool canFly, bool canSwim){
+        if (hasWheels == true || canFly == true || canSwim == true){
+            return "is vehicle";
+        }
+        else (hasWheels == false && canFly == false && canSwim == false){
+            return "";
+        }
+    }
 }
